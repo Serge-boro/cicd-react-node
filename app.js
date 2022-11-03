fbc5066297eba0944526a0a8a9eb5181146c31a4
Serge-boro <serhiiborodin83@gmail.com>
2022-11-02 22:54:32 -0400

    Added github file

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
+// const path = require('path')
 
-app.use(express.json())
+// app.use(express.json())
+
+app.use(express.static('./frondend/build'))
 
 app.get('/names', (req, res) => {
   res.send('Serhii')
 })
 
-app.post('/api/v1', (req, res) => {
-  const { dataInfo } = req.body
-  res.status(200).json({ dataInfo })
-})
+// app.post('/api/v1', (req, res) => {
+//   const { dataInfo } = req.body
+//   res.status(200).json({ dataInfo })
+// })
 
 app.listen(5000, console.log('Listening port 5000'))
