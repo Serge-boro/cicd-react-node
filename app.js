4baa63ab0402b7f0494088842e7e35417403579d
Serge-boro <serhiiborodin83@gmail.com>
2022-11-02 23:22:31 -0400

    Changed code

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-// const path = require('path')
+const path = require('path')
 
 // app.use(express.json())
 
-app.use(express.static('./frondend/build'))
+app.use(express.static(path.join(__dirname, '/frondend/build'))
 
 app.get('/names', (req, res) => {
   res.send('Serhii')
